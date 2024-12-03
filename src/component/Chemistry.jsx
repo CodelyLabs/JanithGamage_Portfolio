2b0237d3465660f0791dbc080ef13be034168e4e
yasiru-vk-721 <yasiru.k721@gmail.com>
2024-12-03 10:18:55 +0530

    Created car components

--- src/component/Chemistry.jsx
+++ src/component/Chemistry.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
-
+import Card from "./Card"
 function Chemistry() {
   return (
-    <div>Chemistry</div>
+    <>
+<div class="bg-red-500 transform rotate-20  h-36 flex items-center justify-center">
+<div class="transform rotate-20 text-white font-bold text-6xl">
+    Chemistry
+  </div>
+</div>
+    <div className='mt-8'>
+      <Card/>
+    </div>
+    </>
   )
 }
 
