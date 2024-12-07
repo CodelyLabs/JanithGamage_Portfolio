7a47c58b025194404632309b126dfbee27c55a98
yasiru-vk-721 <yasiru.k721@gmail.com>
2024-12-07 18:59:07 +0530

    Added polygon shape

--- src/component/Chemistry.jsx
+++ src/component/Chemistry.jsx
@@ -1,18 +1,25 @@
-import React from 'react'
-import Card from "./Card"
+import React from 'react';
+import Card from './Card';
+
 function Chemistry() {
   return (
     <>
-<div class="bg-red-500 transform rotate-20  h-36 flex items-center justify-center">
-<div class="transform rotate-20 text-white font-bold text-6xl">
-    Chemistry
-  </div>
-</div>
-    <div className='mt-8'>
-      <Card/>
-    </div>
+      {/* Banner Section with Polygon Shape */}
+      <div
+        className="bg-red-500 text-white text-center relative h-48 flex items-center justify-center"
+        style={{
+          clipPath: 'polygon(0 0, 100% 0, 100% 100%, 50% 80%, 0 100%)',
+        }}
+      >
+        <h1 className="font-bold text-6xl mb-6">Chemistry</h1>
+      </div>
+
+      {/* Card Section */}
+      <div className="mt-8">
+        <Card />
+      </div>
     </>
-  )
+  );
 }
 
-export default Chemistry
+export default Chemistry;
