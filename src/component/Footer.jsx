155345635fb173ccf2f0a256791dc7165fbfd375
yasiru-vk-721 <yasiru.k721@gmail.com>
2024-12-27 08:07:06 +0530

    Navbar mobile optimized

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,16 +1,67 @@
-import React from 'react'
+import React from 'react';
 import logo from '../assets/Janith Liaya (3.3).png';
+
 function Footer() {
   return (
-    <div className='h-[10rem] w-full bg-[#1C1C1C] text-slate-50 '>
-        <div className='flex space-x-10 mt-10'>
-            <img src={logo} alt="logo" className='w-[10rem]'/>
-           <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, reiciendis.</div>
-           <div> Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, voluptates?</div>
-           <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, architecto!</div>
+    <div className="h-auto w-full bg-[#1C1C1C] text-slate-50 p-4">
+      {/* Main Footer Content */}
+      <div className="flex flex-col lg:flex-row lg:space-x-10 justify-center items-center lg:items-start text-center lg:text-left">
+        
+        {/* Logo */}
+        <img
+          src={logo}
+          alt="logo"
+          className="w-[8rem] lg:w-[10rem] mb-4 lg:mb-0"
+        />
+
+        {/* Chemistry Links */}
+        <div className="text-sm lg:text-base max-w-[16rem] mx-2 mb-4 lg:mb-0">
+          <h4 className="font-semibold mb-2">- Quick Links -</h4>
+          <ul className="space-y-1">
+            <li>
+              <a href="/chemistry/pastpapers" className="hover:underline">
+                Past Papers
+              </a>
+            </li>
+            <li>
+              <a href="/chemistry/markingscheme" className="hover:underline">
+                Marking Scheme
+              </a>
+            </li>
+            <li>
+              <a href="/chemistry/analysis" className="hover:underline">
+                Analysis
+              </a>
+            </li>
+            <li>
+              <a href="/chemistry/mcq" className="hover:underline">
+                Classified MCQs
+              </a>
+            </li>
+            <li>
+              <a href="/chemistry/resourcebooks" className="hover:underline">
+                Resource Books
+              </a>
+            </li>
+          </ul>
         </div>
+
+        {/* Business Links */}
+        <div className="text-sm lg:text-base max-w-[16rem] mx-2 mb-4 lg:mb-0">
+          <p>
+            <a href="/business" className="hover:underline">Business</a>
+          </p>
+        </div>
+
+        {/* Contact Us */}
+        <div className="text-sm lg:text-base max-w-[16rem] mx-2">
+          <p>
+            <a href="/contact" className="hover:underline">About Me</a>
+          </p>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Footer;
